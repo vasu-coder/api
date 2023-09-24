9b5320e25dfa03c42beea9d08b0004f28d68d068
Priyanshu Gupta <priyanshugupta.112002@gmail.com>
2023-09-24 21:59:38 +0530

    hello

--- conn.js
+++ conn.js
@@ -1,19 +1,8 @@
 const mon = require("mongoose");
-<<<<<<< HEAD
-// creating a database
-mon
-  .connect(
-    "mongodb+srv://abhishek:as0709716@cluster0.pe0m0ij.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    console.log("connection Successful");
-  })
-  .catch((error) => {
-=======
 // creating a database 
-mon.connect("mongodb+srv://abhishek:as0709716@cluster0.pe0m0ij.mongodb.net/?retryWrites=true&w=majority").then(()=>{
+mon.connect("mongodb+srv://vishalvasumittal1973:Vasu.2003@cluster0.avinw5c.mongodb.net/?retryWrites=true&w=majority").then(()=>{
     console.log("connection Successful")
 }).catch((error)=>{
->>>>>>> d9822ef9cbad80cfc37c50e4c7d336d5d4db79d5
     console.log(error);
-  });
+})
+
