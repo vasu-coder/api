db9d3104d96ea23b47912be8eec706c1595266bf
vasu-coder <vishal.vasumittal1973@gmail.com>
2023-09-25 20:59:27 +0530

    cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const supervisorSchema = require("./supervisorModel.js");
 const cors = require("cors");
 app.use(
   cors({
-    origin: "https://localhost:3000",
+    origin: "http://localhost:3000",
   })
 );
 require("./conn.js");
